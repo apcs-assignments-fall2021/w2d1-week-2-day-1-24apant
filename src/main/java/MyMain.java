98f66d2f3713fcd31f8f5b3ec858f9cd9a863aa4
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-09 09:55:16 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,20 +3,44 @@ public class MyMain {
     // of the shorter String plus the second half of the longer String.
     public static String halfAdder(String str1, String str2) {
         // REPLACE THIS WITH YOUR CODE
-        return "";
+        String longStr;
+        String shortStr;
+        if (str1.length() > str2.length()){
+            longStr = str1;
+            shortStr = str2;
+        }
+        else{
+            longStr = str2;
+            shortStr = str1;
+        }
+        String second_part = longStr.substring(Math.floorDiv(longStr.length(), 2));
+        String first_part = shortStr.substring(0, Math.floorDiv(shortStr.length(), 2));
+
+        return first_part + second_part;
     }
 
     // Takes a String as input, and returns true if the String contains an 'A' or 'a'
     // and returns false otherwise
     public static boolean checkIfA(String str) {
-        // REPLACE THIS WITH YOUR CODE
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) == 'A' || str.charAt(i) == 'a'){
+                return true;
+            }
+        }
         return false;
     }
 
     //  Takes in two Strings as input and returns a String equal to those two Strings added
     //  together in alphabetic order.
     public static String stringOrder(String str1, String str2) {
-        return "";
+        int comparedStr = str1.toLowerCase().compareTo(str2.toLowerCase());
+        if (comparedStr < 0){
+            //str1 is less
+            return str1 + str2;
+        }
+        else{
+            return str2 + str1;
+        }
     }
 
 
